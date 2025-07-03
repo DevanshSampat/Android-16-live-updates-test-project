dc13b84c14303058846a60854dd6a5d02e767178
Devansh.Sampat <devansh.sampat@cimpress.com>
2025-07-03 23:56:43 +0530

    Added progress centric notification

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.drawable.Icon;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -17,8 +18,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
+import androidx.core.graphics.drawable.IconCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -75,6 +80,21 @@ public class MainActivity extends AppCompatActivity {
                     .setContentTitle("Order Status")
                     .setContentText("Progress: " + progress + "%")
                     .setSmallIcon(android.R.drawable.stat_sys_download_done)
+                    .setStyle(new NotificationCompat.ProgressStyle().setStyledByProgress(false)
+                            .setProgress(456)
+                            .setProgressTrackerIcon(IconCompat.createWithResource(context, android.R.drawable.arrow_up_float))
+                            .setProgressSegments(
+                                    Arrays.asList(new NotificationCompat.ProgressStyle.Segment(41).setColor(Color.GREEN),
+                                            new NotificationCompat.ProgressStyle.Segment(552).setColor(Color.GREEN),
+                                            new  NotificationCompat.ProgressStyle.Segment(253).setColor(Color.GREEN),
+                                            new NotificationCompat.ProgressStyle.Segment(152).setColor(Color.GREEN),
+                                            new  NotificationCompat.ProgressStyle.Segment(153).setColor(Color.GREEN),
+                                            new NotificationCompat.ProgressStyle.Segment(194).setColor(Color.GRAY),
+                                            new NotificationCompat.ProgressStyle.Segment(124).setColor(Color.GRAY)
+                                            )
+
+                            ))
+                    .setShortCriticalText("Placed")
                     .setOngoing(true)
                     .setProgress(100, progress, false)
                     .setRequestPromotedOngoing(true);
